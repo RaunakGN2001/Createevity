b89d768721f6811e1b2e9f5b30653ad06b3637f6
Raunak Gayen <rnk.express@gmail.com>
2022-10-24 02:58:23 +0530

    added progress bar

--- components/Pagination.js
+++ components/Pagination.js
@@ -34,7 +34,7 @@ const Pagination = ({page, pageCount, redirectURL = '/'}) => {
 
   }
   return (
-    <Box padding='2rem' display='flex' justifyContent='center' alignItems='center' gap='1rem'> 
+    <Box padding='2rem' display='flex' justifyContent='center' alignItems='center' gap='1rem' marginBottom='1rem'> 
       {isPrevDisabled() ? <Button width='100px' colorScheme='green' disabled>Previous</Button> : <Button onClick={() => handlePaginate(-1)} width='100px' colorScheme='green'>Previous</Button>}
       {isNextDisabled() ? <Button width='100px' colorScheme='green' disabled>Next</Button> : <Button onClick={() => handlePaginate(1)} width='100px' colorScheme='green'>Next</Button>}
     </Box>
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,18 @@ import { ChakraProvider, Container } from '@chakra-ui/react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Tabbar from '../components/Tabbar'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import '../styles/globals.css'
 
 
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+
+// NProgress.configure({ showSpinner: false })
 
 
 
@@ -15,7 +24,7 @@ function MyApp({ Component, pageProps }) {
         <Navbar />
         <Component {...pageProps} />
       </Container>
-      {/* <Footer /> */}
+      <Footer />
     </ChakraProvider>
   )
 }
